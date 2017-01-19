19f6fd6cdf96a680ca9e9dc513418a5922d762d1
Kev Comedia <kevcomedia@users.noreply.github.com>
2017-01-19 23:15:32 +0800

    Move state to CamperTable

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import React, { Component } from 'react';
 import CamperTable from './CamperTable';
-import { getJSON } from 'jquery';
 // import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.baseUrl = 'https://fcctop100.herokuapp.com/api/fccusers/top/';
-
-    this.state = {
-      camperList: []
-    };
-  }
-
-  populateTable(route) {
-    getJSON(`${this.baseUrl}${route}`)
-      .done(json => this.setState({camperList: json}));
-  }
-
-  componentDidMount() {
-    this.populateTable('recent');
-  }
-
   render() {
     return (
       <div className="App">
@@ -30,9 +10,7 @@ class App extends Component {
           <h1>freeCodeCamp Camper Leaderboard</h1>
         </header>
 
-        <CamperTable
-          camperList={this.state.camperList}
-          populateTable={this.populateTable.bind(this)} />
+        <CamperTable />
       </div>
     );
   }
--- src/CamperTable.js
+++ src/CamperTable.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
+import { getJSON } from 'jquery';
 import CamperRow from './CamperRow';
 import ClickableHeader from './ClickableHeader';
 
 class CamperTable extends Component {
+  constructor(props) {
+    super(props);
+
+    this.baseUrl = 'https://fcctop100.herokuapp.com/api/fccusers/top/';
+    this.state = {
+      camperList: []
+    };
+  }
+
+  populateTable(route) {
+    getJSON(`${this.baseUrl}${route}`)
+      .done(json => this.setState({camperList: json}));
+  }
+
+  componentDidMount() {
+    this.populateTable('recent');
+  }
+
   render() {
     return (
       <table>
@@ -11,15 +30,15 @@ class CamperTable extends Component {
             <th scope='col'>Rank</th>
             <th scope='col'>Camper</th>
             <th scope='col'>
-              <ClickableHeader text={'Points in last 30 days'} route={'recent'} populateTable={this.props.populateTable} />
+              <ClickableHeader text={'Points in last 30 days'} route={'recent'} populateTable={this.populateTable.bind(this)} />
             </th>
             <th scope='col'>
-              <ClickableHeader text={'All time points'} route={'alltime'} populateTable={this.props.populateTable} />
+              <ClickableHeader text={'All time points'} route={'alltime'} populateTable={this.populateTable.bind(this)} />
             </th>
           </tr>
         </thead>
         <tbody>
-          {this.props.camperList.map((camper, i) => <CamperRow camper={camper} rank={i + 1} key={i} />)}
+          {this.state.camperList.map((camper, i) => <CamperRow camper={camper} rank={i + 1} key={i} />)}
         </tbody>
       </table>
     );
